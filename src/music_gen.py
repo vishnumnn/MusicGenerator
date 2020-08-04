9ba1b230296d000c3b2ca10c295fcac7af929336
Vishnu Menon <54286371+vishnumnn@users.noreply.github.com>
2020-08-03 23:45:26 -0500

    added new training and prediction methods

--- src/music_gen.py
+++ src/music_gen.py
@@ -17,6 +17,7 @@ from numpy.random import choice
 ## CONSTANTS
 _DATE_TIME_FORMAT = "%d_%m_%Y_%H_%M_%S"
 _DATETIME = date_and_time = datetime.now().strftime(_DATE_TIME_FORMAT)
+CHOOSING_CUTOFF = 0.7
 
 ## Get notes and rests per instrument from score
 def notesAndRests(score):
@@ -89,13 +90,13 @@ def noteToMidiNumbers(nList):
     data = np.zeros((len(nList), 102))
     for i in range(len(nList)):
         if(nList[i].isRest):
-            data[i,88] = 1
+            data[i,101] = 1
         else:
             pitches = nList[i].pitches
             for e in pitches:
                 data[i,e.midi] = 1
-                ## REMOVE THIS IF YOU WANT TO ENCODE ALL NOTES IN A CHORD NOT JUST THE FIRST
-                break
+                ## Comment the break IF YOU WANT TO ENCODE ALL NOTES IN A CHORD NOT JUST THE FIRST
+                # break
     return data
 
 def getData(score):
@@ -161,7 +162,7 @@ def cleanData(filepaths, sequenceLength):
 
 ## Create the model and train it on the passed data. 
 ## Write the model and weights to a json and hdf5 file respectively.
-def create_and_train_data(Seqs, Labels):
+def create_and_train_model(Seqs, Labels):
     ## Train Model
     model = Sequential()
     model.add(LSTM(
@@ -173,6 +174,40 @@ def create_and_train_data(Seqs, Labels):
     model.add(LSTM(256))
     model.add(Dense(Seqs.shape[2], activation = 'softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+    ## Train on everything except the first 10 samples
+    model.fit(Seqs, Labels, epochs = 60, batch_size = 32)
+
+    # serialize model to JSON
+    model_json = model.to_json()
+    
+    JSON_filepath = os.getcwd() + '''/models/model_{0}.json'''.format(_DATETIME)
+    HDF5_filepath = os.getcwd() + '''/models/weights_{0}.h5'''.format(_DATETIME)
+
+    # write model to json
+    with open(JSON_filepath, "w") as json_file:
+        json_file.write(model_json)
+    print("Wrote model to JSON")
+
+    # serialize weights to HDF5
+    model.save_weights(HDF5_filepath)
+    print("Saved weights to hdf5")
+
+    return (JSON_filepath, HDF5_filepath)
+
+## Creates and trains model on multiple labels on multiple categories
+def create_and_train_model_V2(Seqs, Labels):
+    ## Train Model
+    model = Sequential()
+    model.add(LSTM(
+        256,
+        input_shape=(Seqs.shape[1], Seqs.shape[2]),
+        return_sequences=True
+    ))
+    model.add(LSTM(256, return_sequences=True))
+    model.add(LSTM(256))
+    model.add(Dense(Seqs.shape[2], activation = 'sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
     ## Train on everything except the first 10 samples
     model.fit(Seqs, Labels, epochs = 60, batch_size = 32)
@@ -222,12 +257,44 @@ def predict_with_saved_weights(json_path, h5_path, seed_data, number_of_notes):
         i += 1        
     return predictions
 
+## TODO: Test changing added note to what is there after choosing cutoff is applied
+def predict_with_saved_weights_V2(json_path, h5_path, seed_data, number_of_notes):
+    ## seed_data is a 2 dimensional input (sequence of one-hot-encoded notes)
+    # Load model
+    json_file = open(json_path, 'r')
+    model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(model_json)
+
+    # Load weights into model
+    model.load_weights(h5_path)
+
+    # compile model
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+    inp = seed_data.tolist()
+    predictions = []
+    i = 0
+    while(i < number_of_notes):
+        inpNP = np.asarray(inp)
+        pred = model.predict(np.reshape(inpNP, (1,inpNP.shape[0],inpNP.shape[1])))
+        ## Currently only chooses the maximum of the predicted array for storage
+        inp.append(pred[0])
+        draw = np.argwhere(pred[0] >= CHOOSING_CUTOFF)
+        predictions.append(draw)
+        inp = inp[1:len(inp)]
+        i += 1        
+    return predictions
+
 def create_MIDI_file(predicted_notes):
     s = stream.Stream()
     for m in predicted_notes:
-        p = pitch.Pitch(m)
-        n = note.Note()
-        n.pitch = p
+        if(m == 101):
+            n = note.Rest()
+        else:
+            p = pitch.Pitch(m)
+            n = note.Note()
+            n.pitch = p
         n.duration = duration.Duration(quarterLength = 1)
         s.append(n)
     # speed up piece by factor of 2
